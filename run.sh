46cf2801e9edea79bff17fb06d84e3901e919782
Jimmy Muchachasoy <test>
2021-11-05 18:55:10 -0500

    sin ti

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 DOCKER_IMAGE=markhobson/maven-chrome:jdk-8
 
 docker pull $DOCKER_IMAGE
-docker run --rm -it -v "$PWD":/usr/src -w /usr/src $DOCKER_IMAGE mvn clean test
+docker run --rm -v "$PWD":/usr/src -w /usr/src $DOCKER_IMAGE mvn clean test
